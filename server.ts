74582796f8d81cbcd5516ee3f1fc5ed0b0886fc4
Nick Richardson <90582249+nicjrichDEV@users.noreply.github.com>
2025-12-30 19:01:36 -0500

    Moved openai logic to server. Route is setup just needs tested.

--- server.ts
+++ server.ts
@@ -1,9 +1,55 @@
 import index from "./index.html";
+import OpenAI from "openai";
+
+const openai = new OpenAI();
+
+type TranslateRequest = {
+  text: string;
+  language: string;
+};
 
 Bun.serve({
-  routes: { "/": index, "/api/translate": {} },
+  routes: {
+    "/": index,
+    "/api/translate": {
+      POST: async (req) => {
+        const { text, language } = (await req.json()) as TranslateRequest;
+        console.log(`Translating: ${text} to ${language}`);
+
+        const translation = await getTranslation(text, language);
+
+        return Response.json({ message: translation });
+      },
+    },
+  },
   development: {
     hmr: true,
     console: true,
   },
 });
+
+async function getTranslation(lang: string, text: string) {
+  try {
+    const response = await openai.responses.create({
+      model: "gpt-5-nano",
+      input: [
+        {
+          role: "developer",
+          content: `You are a language pollyglot. You will receive a request to translate a phrase from english into a user specified language
+          
+          ### Example Input
+          english -> {USER_SPECIFIED_LANGUAGE} | {USER_SPECIFIED__PHRASE}
+          `,
+        },
+        {
+          role: "user",
+          content: `english -> ${lang} | ${text}`,
+        },
+      ],
+    });
+
+    return response;
+  } catch (error) {
+    throw new Error(`There was an issue with your request: ${error}`);
+  }
+}
